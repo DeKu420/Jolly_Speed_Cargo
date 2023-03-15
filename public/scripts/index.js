772a9e5a9c92364d7de89bf4dcdfd7dc8bdfc419
DeKu420 <kevinjohn0805@gmail.com>
2023-03-15 14:29:37 +0530

    Navbar fixed

---
+++ public/scripts/index.js
@@ -0,0 +1,7 @@
+const ham = document.getElementById("hamburger");
+const navBar = document.querySelector("#navBar");
+ham.addEventListener("click",function(){
+    navBar.classList.toggle(
+        "open"
+    );
+})
