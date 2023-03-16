ea92b7d7d60a87bda5204bd17284115ffc81c51d
DeKu420 <kevinjohn0805@gmail.com>
2023-03-17 02:14:18 +0530

    16/3/23 - EOD fixed

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,6 +1,5 @@
 const ham = document.getElementById("hamburger");
 const navBar = document.querySelector("#navBar");
-console.log(ham, navBar);
 ham.addEventListener("click", function () {
   navBar.classList.toggle("open");
 });
