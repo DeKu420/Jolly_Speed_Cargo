b6d311bb2b6ebd4efe9358a11f136f2d42a28558
DeKu420 <kevinjohn0805@gmail.com>
2023-03-19 20:23:08 +0530

    Final

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,15 +1,28 @@
 const tbody = document.querySelector("tbody");
+const filterSel = document.getElementById("filter");
+let mainData;
+
+filterSel.addEventListener("change", function () {
+  if (filterSel.value == "") {
+    displayData(mainData);
+  } else {
+    let filterData = mainData.filter(function (item) {
+      return item.status == filterSel.value;
+    });
+    displayData(filterData);
+  }
+});
 
 const getData = async function () {
   let response = await fetch("http://localhost:8000/booking/get");
   let data = await response.json();
   console.log(data);
   displayData(data);
+  mainData = data;
 };
 
 const displayData = (data) => {
   tbody.innerHTML = "";
-  tbody.innerHTML = `<th>Name</th><th>Email</th><th>Pickup</th><th>Destination</th><th>Service</th><th>Phone</th><th>Status</th>`;
   data.forEach((el) => {
     let tr = document.createElement("tr");
 
@@ -40,9 +53,17 @@ const displayData = (data) => {
         : el.status == 3
         ? "Delivered"
         : "Enquired";
+    td9.style.backgroundColor =
+      el.status == 1
+        ? "#F9ED69"
+        : el.status == 2
+        ? "#F08A5D"
+        : el.status == 3
+        ? "#B83B5E"
+        : "#6A2C70";
 
     let td7 = document.createElement("td");
-    td7.innerHTML = "Edit";
+    td7.innerHTML = `<img src="https://img.icons8.com/sf-black/24/null/edit-row.png"/>`;
     td7.style.cursor = "pointer";
     td7.addEventListener("click", function () {
       dial.showModal();
@@ -62,7 +83,7 @@ const displayData = (data) => {
     });
 
     let td8 = document.createElement("td");
-    td8.innerHTML = "Delete";
+    td8.innerHTML = `<img src="https://img.icons8.com/material-rounded/24/FFFFFF/delete-trash.png"/>`;
     td8.style.cursor = "pointer";
     td8.style.backgroundColor = "red";
     td8.style.color = "white";
@@ -111,10 +132,10 @@ const displayUserData = (data) => {
     td4.innerHTML = el.admin;
 
     let td5 = document.createElement("td");
-    td5.innerHTML = "Edit";
+    td5.innerHTML = `<img src="https://img.icons8.com/sf-black/24/null/edit-row.png"/>`;
 
     let td6 = document.createElement("td");
-    td6.innerHTML = "Delete";
+    td6.innerHTML = `<img src="https://img.icons8.com/material-rounded/24/FFFFFF/delete-trash.png"/>`;
     td6.style.backgroundColor = "red";
     td6.style.color = "white";
 
